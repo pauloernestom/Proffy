a482746a04a3b2aa707e25130e1c346c1cba4a44
Paulo Ernesto Marchezi <pauloernestom@gmail.com>
2020-08-06 14:52:02 -0300

    Paginas estudas mobile finalizadas

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -16,6 +16,11 @@ function Landing() {
     function hadleNavigateToGiveClassesPage() {
         navigate('GiveClasses')
     }
+
+    function handleNavigateToSudyPages() {
+        navigate('Study');
+    }
+
     return (
         <View style={styles.container}>
             <Image source={landingImg} style={styles.banner} />
@@ -26,7 +31,7 @@ function Landing() {
             </Text>
 
             <View style={styles.buttonsContainer}>
-                <RectButton style={[styles.button, styles.buttonPrimary]}>
+                <RectButton onPress={handleNavigateToSudyPages} style={[styles.button, styles.buttonPrimary]}>
                     <Image source={studyIcon}/>
 
                     <Text style={styles.buttonText}>Estudar</Text>
